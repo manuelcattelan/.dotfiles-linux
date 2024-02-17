7fb72da40066034f225c0d504f81b0d0dd767291
manuelcattelan <cattelanmanuel07@gmail.com>
2024-02-17 11:39:21 +0100

    feat(nvim): harpoon plugin

---
+++ .config/nvim/lua/mcattelan/plugins/harpoon.lua
@@ -0,0 +1,22 @@
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = function()
+    local harpoon = require("harpoon")
+    -- stylua: ignore
+    return {
+      { "<leader>ha", function() harpoon:list():append() end, desc = "Append file to harpoon list" },
+      { "<leader>hw", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "Toggle harpoon menu with harpoon list" },
+      { "<leader>h1", function() harpoon:list():select(1) end, desc = "Open first buffer in harpoon list" },
+      { "<leader>h2", function() harpoon:list():select(2) end, desc = "Open second buffer in harpoon list" },
+      { "<leader>h3", function() harpoon:list():select(3) end, desc = "Open third buffer in harpoon list" },
+      { "<leader>h4", function() harpoon:list():select(4) end, desc = "Open fourth buffer in harpoon list" },
+      { "<C-S-P>", function() harpoon:list():prev() end, desc = "Toggle previous buffer in harpoon list" },
+      { "<C-S-N>", function() harpoon:list():next() end, desc = "Toggle next buffer in harpoon list" },
+    }
+  end,
+  config = function()
+    require("harpoon").setup()
+  end,
+}
