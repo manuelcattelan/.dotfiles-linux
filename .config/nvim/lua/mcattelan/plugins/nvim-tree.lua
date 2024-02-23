ce8a689213874c18aacac1f010290dcacab3a8b7
manuelcattelan <cattelanmanuel07@gmail.com>
2024-02-23 14:29:06 +0100

    feat(nvim): more keymaps for nvim-tree

--- .config/nvim/lua/mcattelan/plugins/nvim-tree.lua
+++ .config/nvim/lua/mcattelan/plugins/nvim-tree.lua
@@ -5,17 +5,11 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
+  -- stylua: ignore
   keys = {
-    {
-      "<leader>tt",
-      "<cmd>NvimTreeToggle<CR>",
-      desc = "Toggle file explorer",
-    },
-    {
-      "<leader>tf",
-      "<cmd>NvimTreeFindFile<CR>",
-      desc = "Toggle file explorer and focus current file",
-    },
+    { "<leader>tt", "<cmd>NvimTreeToggle<CR>", desc = "Toggle file explorer" },
+    { "<leader>tf", "<cmd>NvimTreeFindFile<CR>", desc = "Toggle file explorer and focus current file" },
+    { "<leader>to", "<cmd>NvimTreeCollapse<CR>", desc = "Toggle file explorer and focus current file" },
   },
   opts = {
     sort = {
